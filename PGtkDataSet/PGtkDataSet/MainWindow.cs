3b3e09a1e9f9a7a9a50bdc0eed26884c2900a445
Javiersanchez <javiersanchezflores17@gmail.com>
2013-01-11 09:45:10 +0100

    PGtkDataSet Versión 2

--- PGtkDataSet/PGtkDataSet/MainWindow.cs
+++ PGtkDataSet/PGtkDataSet/MainWindow.cs
@@ -1,8 +1,8 @@
 using Gtk;
 using Npgsql;
 using System;
-using System.Data.SqlClient;
 using System.Data;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 public partial class MainWindow: Gtk.Window
@@ -32,5 +32,30 @@ public partial class MainWindow: Gtk.Window
 		dbDataAdapter.Fill (dataSet);
 		
 		Console.WriteLine("Tables.Count={0}, dataSet.Tables.Count");
+		
+		foreach (DataTable dataTable in dataSet.Tables){
+			show (dataTable);
+			
+		DataRow dataRow = dataSet.Tables[0].Rows[0];
+		dataRow["nombre"] = DateTime.Now.ToString();
+			
+		Console.WriteLine("Tabla con los cambios:");
+			show (dataSet.Tables[0]);
+			
+		dbDataAdapter.Update (dataSet);	
+		}
+	}
+	
+	private void show(DataTable dataTable){
+//		foreach (DataColumn dataColumn in dataTable.Columns)
+//			Console.WriteLine("Column.Name={0})", dataColumn.ColumnName);
+		
+		foreach (DataRow dataRow in dataTable.Rows){
+			foreach (DataColumn dataColumn in dataTable.Columns)
+				Console.Write("[{0}={1}] ", dataColumn.ColumnName, dataRow[dataColumn]);
+			Console.WriteLine();
+		}
+			
+	
 	}
 }
