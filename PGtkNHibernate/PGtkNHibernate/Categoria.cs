992c7f6a9ffba83f08d7318fc65d09de23a10bd9
Javiersanchez <javiersanchezflores17@gmail.com>
2013-02-01 10:42:59 +0100

    Añadiendo PGtkNHibernate version 1

---
+++ PGtkNHibernate/PGtkNHibernate/Categoria.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace PGtkNHibernate
+{
+	public class Categoria
+	{
+			public virtual long Id {get; set;}
+			public virtual string Nombre {get; set;}
+			                
+			//private string nombre;
+			//public string Nombre{
+			//get {return nombre;}
+			//set {nombre = value;}
+			//}
+	}
+}
+
---
+++ PGtkNHibernate/PGtkNHibernate/MainWindow.cs
@@ -0,0 +1,40 @@
+using System;
+using Gtk;
+using NHibernate.Cfg;
+using PGtkNHibernate;
+
+public partial class MainWindow: Gtk.Window
+{	
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		
+		Configuration configuration = new Configuration();
+		configuration.Configure();
+		
+		configuration.AddAssembly(typeof(Categoria).Assembly);
+		new SchemaExport(configuration).Execute(true, false, false);
+		
+		ISessionFactory sessionFactory = configuration.BuildSessionFactory();
+		
+		ISession session = sessionFactory.OpenSession();
+		
+		Categoria categoria = (Categoria)session.Load(typeof(Categoria), 2L);
+		
+		Console.WriteLine("Categoria Id={0} Nombre={1}");
+		
+		categoria.Nombre = DateTime.Now.ToString();
+		
+		session.SaveOrUpdate (categoria);
+		
+		session.Close();
+		
+		sessionFactory.Close();
+	}
+	
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+}
