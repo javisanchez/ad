d1f88be2f2a46330dc2b85239f610fec3fbffa71
Javiersanchez <javiersanchezflores17@gmail.com>
2013-02-06 09:48:21 +0100

    Modificación PGtkNHibernate Version 3 (Update y insert)

--- PGtkNHibernate/PGtkNHibernate/MainWindow.cs
+++ PGtkNHibernate/PGtkNHibernate/MainWindow.cs
@@ -1,6 +1,11 @@
 using System;
 using Gtk;
+
+using NHibernate;
+
 using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
 using PGtkNHibernate;
 
 public partial class MainWindow: Gtk.Window
@@ -17,22 +22,47 @@ public partial class MainWindow: Gtk.Window
 		
 		ISessionFactory sessionFactory = configuration.BuildSessionFactory();
 		
-		ISession session = sessionFactory.OpenSession();
-		
-		Categoria categoria = (Categoria)session.Load(typeof(Categoria), 2L);
-		
-		Console.WriteLine("Categoria Id={0} Nombre={1}");
-		
-		categoria.Nombre = DateTime.Now.ToString();
-		
-		session.SaveOrUpdate (categoria);
-		
-		session.Flush();
-		
-		session.Close();
+		updateCategoria(sessionFactory);
+			
+		insertCategoria(sessionFactory);
 		
 		sessionFactory.Close();
 	}
+	
+	private void updateCategoria(ISessionFactory sessionFactory){
+		ISession session = sessionFactory.OpenSession();//Esta linea debe estar dentro ya que si hay alguna excepción no se ejecutará y el close siempre se ejecutará.
+		try{
+			Categoria categoria = (Categoria)session.Load(typeof(Categoria), 2L);
+			Console.WriteLine("Categoria Id={0} Nombre={1}");		
+			categoria.Nombre = DateTime.Now.ToString();		
+			session.SaveOrUpdate (categoria);		
+			session.Flush();
+		}finally{//Lo que está dentro del finally SIEMPRE se va a ejecutar
+		session.Close();
+		}
+	}
+	
+	//Usando el using; realizas la versión corta del try{ }finally{ } (Implementa la interfaz disposable)
+	private void insertCategoria(ISessionFactory sessionFactory){
+		using (ISession session = sessionFactory.OpenSession()){
+			Categoria categoria = new Categoria();
+			Categoria.Nombre = "Nueva " + DateTime.Now.ToString();
+			session.SaveOrUpdate (categoria);		
+			session.Flush();	
+		}
+	}
+	
+//	private void insertCategoria(ISessionFactory sessionFactory){
+//		ISession session = sessionFactory.OpenSession();
+//		try{
+//			Categoria categoria = new Categoria();
+//			Categoria.Nombre = "Nueva " + DateTime.Now.ToString();
+//			session.SaveOrUpdate (categoria);		
+//			session.Flush();	
+//		}finally{	
+//		session.Close();
+//		}
+//	}
 	
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
--- PGtkNHibernate/PGtkNHibernate/gtk-gui/MainWindow.cs
+++ PGtkNHibernate/PGtkNHibernate/gtk-gui/MainWindow.cs
@@ -4,8 +4,6 @@
 public partial class MainWindow
 {
 	private global::Gtk.UIManager UIManager;
-	private global::Gtk.VBox vbox1;
-	private global::Gtk.Toolbar toolbar1;
 	
 	protected virtual void Build ()
 	{
@@ -18,26 +16,11 @@ public partial class MainWindow
 		this.Name = "MainWindow";
 		this.Title = global::Mono.Unix.Catalog.GetString ("MainWindow");
 		this.WindowPosition = ((global::Gtk.WindowPosition)(4));
-		// Container child MainWindow.Gtk.Container+ContainerChild
-		this.vbox1 = new global::Gtk.VBox ();
-		this.vbox1.Name = "vbox1";
-		this.vbox1.Spacing = 6;
-		// Container child vbox1.Gtk.Box+BoxChild
-		this.UIManager.AddUiFromString ("<ui><toolbar name='toolbar1'/></ui>");
-		this.toolbar1 = ((global::Gtk.Toolbar)(this.UIManager.GetWidget ("/toolbar1")));
-		this.toolbar1.Name = "toolbar1";
-		this.toolbar1.ShowArrow = false;
-		this.vbox1.Add (this.toolbar1);
-		global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.toolbar1]));
-		w2.Position = 0;
-		w2.Expand = false;
-		w2.Fill = false;
-		this.Add (this.vbox1);
 		if ((this.Child != null)) {
 			this.Child.ShowAll ();
 		}
-		this.DefaultWidth = 400;
-		this.DefaultHeight = 300;
+		this.DefaultWidth = 397;
+		this.DefaultHeight = 359;
 		this.Show ();
 		this.DeleteEvent += new global::Gtk.DeleteEventHandler (this.OnDeleteEvent);
 	}
